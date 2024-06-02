0a04a509d5a24cf8ff0ea82bb59fc444de801275
Nabila Tajrin <nabilatajrin@users.noreply.github.com>
2024-06-02 10:19:51 +0600

    Update 06-leveraging-attributes-on-namedtuples.py

--- 08-data-types-for-data-science-in-python/04-advanced-data-types/06-leveraging-attributes-on-namedtuples.py
+++ 08-data-types-for-data-science-in-python/04-advanced-data-types/06-leveraging-attributes-on-namedtuples.py
@@ -1,5 +1,5 @@
 # Iterate over the first twenty entries in labeled_entries
-for s in labeled_entries:
+for entry in labeled_entries[:20]:
     # if the entry's species equals Chinstrap
     if s.species == 'Chinstrap':
       # Print each entry's sex and body_mass separated by a colon
